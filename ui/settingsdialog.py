3f613e984086cd6e55f7ec55e791fd6cdc030b7c
Rexolt <157226862+Rexolt@users.noreply.github.com>
2025-02-25 20:39:40 +0100

    Style picker

--- ui/settingsdialog.py
+++ ui/settingsdialog.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QSlider, QCheckBox, QPushButton, QFrame
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QSlider, QCheckBox, QPushButton, QFrame, QComboBox
 from PyQt5.QtCore import Qt
 
 class SettingsDialog(QDialog):
@@ -11,6 +11,7 @@ class SettingsDialog(QDialog):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
+      
         f = QFrame()
         h = QHBoxLayout()
         f.setLayout(h)
@@ -26,6 +27,18 @@ class SettingsDialog(QDialog):
             h.addWidget(s)
             self.eq_sliders.append(s)
 
+       
+        style_layout = QHBoxLayout()
+        lbl_style = QLabel("Stílus:")
+        self.styleComboBox = QComboBox()
+        self.styleComboBox.addItem("Windows 95 Retro")
+        self.styleComboBox.addItem("Klasszikus")
+        self.styleComboBox.addItem("Sötét")
+        style_layout.addWidget(lbl_style)
+        style_layout.addWidget(self.styleComboBox)
+        layout.addLayout(style_layout)
+
+    
         self.chk_shuffle = QCheckBox("Shuffle")
         self.chk_shuffle.setChecked(self.config_manager.get_shuffle())
         self.chk_repeat = QCheckBox("Repeat")
@@ -33,6 +46,7 @@ class SettingsDialog(QDialog):
         layout.addWidget(self.chk_shuffle)
         layout.addWidget(self.chk_repeat)
 
+       
         vol_layout = QHBoxLayout()
         lbl_vol = QLabel("Hangerő:")
         self.slider_volume = QSlider(Qt.Horizontal)
@@ -42,6 +56,7 @@ class SettingsDialog(QDialog):
         vol_layout.addWidget(self.slider_volume)
         layout.addLayout(vol_layout)
 
+        
         btn_layout = QHBoxLayout()
         btn_ok = QPushButton("OK")
         btn_cancel = QPushButton("Mégse")
@@ -54,6 +69,7 @@ class SettingsDialog(QDialog):
         self.slider_volume.valueChanged.connect(self.audio_manager.set_volume)
 
     def accept(self):
+       
         if self.chk_shuffle.isChecked():
             self.audio_manager.shuffle_on()
             self.config_manager.set_shuffle(True)
@@ -61,6 +77,7 @@ class SettingsDialog(QDialog):
             self.audio_manager.shuffle_off()
             self.config_manager.set_shuffle(False)
 
+       
         if self.chk_repeat.isChecked():
             self.audio_manager.repeat_on()
             self.config_manager.set_repeat(True)
@@ -68,11 +85,33 @@ class SettingsDialog(QDialog):
             self.audio_manager.repeat_off()
             self.config_manager.set_repeat(False)
 
+        
         v = self.slider_volume.value()
         self.config_manager.set_volume(v)
         self.audio_manager.set_volume(v)
 
+        
         eq_vals = [s.value() for s in self.eq_sliders]
         self.config_manager.set_eq_values(eq_vals)
+
+        
+        selected_style = self.styleComboBox.currentText()
+        if selected_style == "Windows 95 Retro":
+            qss_file = "resources/skins/win95_dark_v2.qss"
+        elif selected_style == "Klasszikus":
+            qss_file = "resources/skins/classic.qss"
+        elif selected_style == "modern":
+            qss_file = "resources/skins/modern.qss"
+        else:
+            qss_file = ""
+
+        if qss_file:
+            try:
+                with open(qss_file, "r", encoding="utf-8") as f:
+                    qss = f.read()
+                    if self.parent():
+                        self.parent().setStyleSheet(qss)
+            except Exception as e:
+                print("Hiba a stílus betöltésekor:", e)
 
         super().accept()
